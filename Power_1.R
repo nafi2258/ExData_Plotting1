5c8f85a45263045113d60a147f5e9e821378966a
nafi2258 <nafidist@iut-dhaka.edu>
2020-02-01 20:23:59 +0600

    Add files via upload

---
+++ Power_1.R
@@ -0,0 +1,7 @@
+data<-read.csv('household_power_consumption.txt',skip=1, sep=";")
+names(data) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+subpower <- subset(data,data$Date=="1/2/2007" | data$Date =="2/2/2007")
+plot_1<-hist(as.numeric(as.character(subpower$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+title(main="Global Active Power")
+dev.copy(png,file="plot_1.png")
+dev.off()
